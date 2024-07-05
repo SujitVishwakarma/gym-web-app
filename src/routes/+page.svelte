5430b9d5aec0b0453248f1a841df08f770554bde
Sujit Vishwakarma <vishwakarma.sujit99@gmail.com>
2024-07-05 14:34:45 +0530

    firsr commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Conversion from "../components/Conversion.svelte";
+import FaQs from "../components/FAQs.svelte";
+import Hero from "../components/Hero.svelte";
+  import Product from "../components/Product.svelte";
+
+</script>
+<main class="flex flex-col">
+    <Hero/>
+    <Product/>
+    <FaQs/>
+    <Conversion/>
+</main>
